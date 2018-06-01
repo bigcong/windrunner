21e4e407e49aba370cd7d4c7327af4f31a58c748
Your Name <you@example.com>
2018-06-01 12:08:09 +0800

    cc

---
+++ src/main/java/com/raysmond/blog/forms/PostForm.java
@@ -0,0 +1,103 @@
+package com.raysmond.blog.forms;
+
+import com.raysmond.blog.models.support.PostFormat;
+import com.raysmond.blog.models.support.PostStatus;
+import com.raysmond.blog.models.support.PostType;
+
+import lombok.Data;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Raysmond
+ */
+public class PostForm {
+    @NotEmpty
+    private String title;
+
+    @NotEmpty
+    private String content;
+
+    private String summary;
+
+    @NotNull
+    private PostFormat postFormat;
+
+    @NotNull
+    private PostStatus postStatus;
+
+    @NotNull
+    private String permalink;
+
+    @NotNull
+    private String postTags;
+
+    @NotNull
+    private PostType postType;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public PostFormat getPostFormat() {
+        return postFormat;
+    }
+
+    public void setPostFormat(PostFormat postFormat) {
+        this.postFormat = postFormat;
+    }
+
+    public PostStatus getPostStatus() {
+        return postStatus;
+    }
+
+    public void setPostStatus(PostStatus postStatus) {
+        this.postStatus = postStatus;
+    }
+
+    public String getPermalink() {
+        return permalink;
+    }
+
+    public void setPermalink(String permalink) {
+        this.permalink = permalink;
+    }
+
+    public String getPostTags() {
+        return postTags;
+    }
+
+    public void setPostTags(String postTags) {
+        this.postTags = postTags;
+    }
+
+    public PostType getPostType() {
+        return postType;
+    }
+
+    public void setPostType(PostType postType) {
+        this.postType = postType;
+    }
+}
